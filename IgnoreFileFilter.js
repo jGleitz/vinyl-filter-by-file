58fbbf3a2e4172f03757f0287e121a4a4f1c361c
Joshua Gleitze <dev@joshuagleitze.de>
2017-03-15 16:45:19 +0100

    vinyl ignorefile filter

---
+++ IgnoreFileFilter.js
@@ -0,0 +1,131 @@
+import path from 'path';
+import ignore from 'ignore';
+import promisify from 'promisify-node';
+
+const fs = promisify('fs');
+
+const NEVER = () => false;
+const ALWAYS = () => true;
+
+/**
+ * Takes an error and returns null, if its a “no such file” error. Throws the error otherwise.
+ *
+ * @param {Error} error
+ *		The error to check.
+ * @returns {null} null if the error’s code is 'ENOENT'.
+ * @throws the error if its code isn’t 'ENOENT'.
+ */
+function nullIfNonExistant(error) {
+	if (error.code === 'ENOENT') {
+		return null;
+	}
+	throw error;
+}
+
+/**
+ * A filter that can be used to check whether files should be included based on gitignore-like files in the directory tree.
+ */
+export default class IgnoreFileFilter {
+
+	/**
+	 * Creates a new filter out of the provided options.
+	 *
+	 * @param {Object} options An option object, having the following members:
+	 * name | type | description
+	 * --- | --- | ---
+	 * filenames | array of strings | filenames of ignore files to consider
+	 */
+	constructor(options) {
+		this.cache = {};
+		this.filenames = options.filename;
+	}
+
+	/**
+	 * Checks whether the provided path should be included.
+	 *
+	 * @param {string} filePath
+	 *		The path to check.
+	 * @param {string} maxParent
+	 *		Path to the maximum parent. Only ignore files in or below this folder will be considered.
+	 * @returns {Promise} A promise for a boolean indicating whether the file should be included (true) or excluded (false).
+	 */
+	check(filePath, maxParent) {
+		return this.checkerFor(path.dirname(filePath), maxParent)
+			.then(checker => checker(filePath));
+	}
+
+	/**
+	 * Creates a checker function that can be used to check (probably deeply nested) files in the provided folder for inclusion. The checker considers all ignore files in the provided folder and above it (up to maxParent), but none below it.
+	 *
+	 * @param {string} folder
+	 *		Path to the folder to create the checker for.
+	 * @param {string} maxParent
+	 *		Path to the maximum parent. Only ignore files in or below this folder will be considered by the created checker.
+	 * @returns {Promise} A promise for the checker function described above.
+	 */
+	checkerFor(folder, maxParent) {
+		if (this.cache[folder]) {
+			return Promise.resolve(this.cache[folder]);
+		} else {
+			if (!folder.startsWith(maxParent)) {
+				return Promise.reject(new Error(`${folder} is outside of maxParent (${maxParent}).`));
+			}
+			let chain;
+			if (folder === maxParent) {
+				// no parent folder, result depends only on the file in this folder
+				chain = this.checkerFromFilesIn(folder);
+			} else {
+				// if this folder’s parent excludes this folder, we can inconditionally exclude all files in this folder
+				chain = this.checkerFor(path.dirname(folder), maxParent)
+					.then(parentChecker => {
+						if (!parentChecker(folder)) {
+							return NEVER;
+						}
+						return this.checkerFromFilesIn(folder)
+							.then(folderChecker => file => folderChecker(file) && parentChecker(file));
+					});
+			}
+			return chain.then(checker => this.cache[folder] = checker);
+		}
+	}
+
+	/**
+	 * Creates a checker function that can be used to check (probably deeply nested) files in the provided folder for inclusion. The checker only considers ignore files in the provided folder.
+	 *
+	 * @param {string} folder
+	 *		Path to the folder to read the ignore files from.
+	 * @returns {Promise} A promise for the checker function described above.
+	 */
+	checkerFromFilesIn(folder) {
+		// read all possible ignore files. If one does not exist, simply return null for its contents
+		return Promise.all(
+			this.filenames.map(filename => fs.readFile(path.join(folder, filename)).catch(nullIfNonExistant))
+		)
+			.then(contents => {
+				let ignores = null;
+				for (const lines of contents) {
+					if (lines !== null) {
+						if (ignores === null) ignores = ignore();
+						ignores.add(lines.toString());
+					}
+				}
+				if (ignores === null) {
+					// there was not a single ignore file. We can inconditionally include all files in this folder.
+					return ALWAYS;
+				}
+				// there was at least one ignore file. Create a filter out of it and use it.
+				const ignoreFilter = ignores.createFilter();
+				return file => ignoreFilter(path.relative(folder, file));
+			});
+	}
+
+	/**
+	 * Frees cached data.
+	 *
+	 * @returns {Promise} A promise for the cleanup’s success.
+	 */
+	cleanup() {
+		this.cache = null;
+		return Promise.resolve();
+	}
+}
---
+++ index.js
@@ -0,0 +1,74 @@
+import {PluginError} from 'gulp-util';
+import through from 'through2';
+import path from 'path';
+import IgnoreFileFilter from './IgnoreFileFilter';
+
+const PLUGIN_NAME = 'gulp-ignorefile';
+const DEFAULTS = {
+	filename: '.ignore',
+	maxParent: file => file.base,
+	excludeIgnoreFile: true
+};
+
+function transformError(callback) {
+	return error => callback(new PluginError(PLUGIN_NAME, error));
+}
+
+function consistsOf(array, type) {
+	for (const element of array) {
+		if (typeof element !== type) {
+			return false;
+		}
+	}
+	return true;
+}
+
+/**
+ * @param {Object?} options:
+ * name | function | type | default
+ * --- | --- | --- | ---
+ * filename | name of ignore files | string or array of strings | '.ignore'
+ * maxParent | highest folder to traverse up to | path or a function vinylobject => path | file => file.base
+ * excludeIgnoreFile | whether to exclude the ignore files | true
+ * @returns {function} the stream stage
+ */
+export default function fileignore(options) {
+
+	const pluginoptions = Object.assign({}, DEFAULTS, options);
+	// option variants transformation and type checks
+	if (typeof pluginoptions.filename === 'string') {
+		pluginoptions.filename = [pluginoptions.filename];
+	} else if (!Array.isArray(pluginoptions.filename)) {
+		throw new PluginError(PLUGIN_NAME, `options.filename must be a string or a not-empty string array. Got ${typeof pluginoptions.filename}.`);
+	} else if (pluginoptions.filename.length === 0) {
+		throw new PluginError(PLUGIN_NAME, 'options.filename may not be empty');
+	} else if (!consistsOf(pluginoptions.filename, 'string')) {
+		throw new PluginError(PLUGIN_NAME, `options.filename may only contain strings. Got ${JSON.stringify(pluginoptions.filename)}.`);
+	}
+	if (typeof pluginoptions.maxParent === 'string') {
+		const parent = pluginoptions.maxParent;
+		pluginoptions.maxParent = () => parent;
+	} else if (typeof pluginoptions.maxParent !== 'function') {
+		throw new PluginError(PLUGIN_NAME, `options.maxParent must be a function or a string. Got ${typeof pluginoptions.maxParent}`);
+	}
+	if (typeof pluginoptions.excludeIgnoreFile !== 'boolean') {
+		throw new PluginError(PLUGIN_NAME, `options.excludeIgnoreFile must be a boolean. Got ${typeof pluginoptions.maxParent}`);
+	}
+
+	const filter = new IgnoreFileFilter(pluginoptions);
+
+	return through.obj((file, encoding, callback) => {
+		// check whether the file is an ignore file and should be excluded
+		if (pluginoptions.excludeIgnoreFile && pluginoptions.filename.indexOf(path.basename(file.path)) !== -1) {
+			callback(null);
+			return;
+		}
+
+		filter.check(file.path, pluginoptions.maxParent(file))
+			.then(result => result ? callback(null, file) : callback(null))
+			.catch(transformError(callback));
+	},
+	callback => filter.cleanup()
+		.then(() => callback(null))
+		.catch(transformError(callback)));
+}
